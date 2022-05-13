49c7db730d10ee22e6f3a826554418da4f954606
gbtb16 <gabrieltavares.tic@gmail.com>
2022-05-12 22:20:45 -0300

    app: updated to v1.0.3+7.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:sasa_bank/routes/global_route.dart';
+import 'package:sasa_bank/src/routes/global_route.dart';
 
 void main() {
   runApp(const GlobalRoute());
